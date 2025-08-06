7cb9970703914b1c5648a6314851399e1a96ef74
bibibi0201 <berlyy0201@gmail.com>
2025-08-06 16:01:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/webhook', async (req, res) => {
     let reply = '';
 
     // ---------- STATUS ----------
-    if (userMessage === 'status') {
+    if (userMessage === 'status.trim()') {
       const statusRes = await fetch(msgUrl);
       const statusData = await statusRes.json();
 
@@ -179,3 +179,4 @@ app.post('/webhook', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
